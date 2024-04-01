995c02f4390164ca9004e9ac848e803f350847f7
zhangwen.chen <623025156@qq.com>
2024-04-01 22:11:09 +0800

    feat: add flat Iterative solution

--- js/simple/flat.js
+++ js/simple/flat.js
@@ -1,18 +1,35 @@
-const arr = [1, [2], [3, [4]]];
+// Recursive solution
 // function flat(arr, depth = 1) {
 //   let res = [];
 //   arr.forEach(item => {
 //     if (Array.isArray(item) && depth > 0) {
-//       res = [...res, ...flat(item, depth - 1)];
+//       // res = [...res, ...flat(item, depth - 1)];
+//       res = res.concat(flat(item, depth - 1));
 //     } else {
 //       res.push(item)
 //     }
 //   })
 //   return res;
 // }
+// Recursive solution by reduce
+// function flat(arr, depth = 1) {
+//   return depth ? arr.reduce((pre, cur) => pre.concat(Array.isArray(cur) ? flat(cur, depth - 1) : cur), []) : arr;
+// }
+// Iterative solution
 function flat(arr, depth = 1) {
-  return depth ? arr.reduce((pre, cur) => pre.concat(Array.isArray(cur) ? flat(cur, depth - 1) : cur), []) : arr;
+  let queue = arr.map(item => [item, depth]);
+  let res = [];
+  while (queue.length > 0) {
+    const [item, itemDepth] = queue.shift();
+    if (Array.isArray(item) && itemDepth > 0) {
+      queue.push(...item.map(i => [i, itemDepth - 1]))
+    } else {
+      res.push(item);
+    }
+  }
+  return res;
 }
+const arr = [1, [2], [3, [4]]];
 flat(arr)
 console.log("🚀 ~ flat(arr):", flat(arr))
 // [1, 2, 3, [4]]
