5ac55d6d5427a9b1182ea4831b68a6daf4bbc148
zhangwen.chen <623025156@qq.com>
2024-04-01 21:00:36 +0800

    feat: add flat

---
+++ js/simple/flat.js
@@ -0,0 +1,28 @@
+const arr = [1, [2], [3, [4]]];
+// function flat(arr, depth = 1) {
+//   let res = [];
+//   arr.forEach(item => {
+//     if (Array.isArray(item) && depth > 0) {
+//       res = [...res, ...flat(item, depth - 1)];
+//     } else {
+//       res.push(item)
+//     }
+//   })
+//   return res;
+// }
+function flat(arr, depth = 1) {
+  return depth ? arr.reduce((pre, cur) => {
+    return [...pre, ...(Array.isArray(cur) ? flat(cur, depth - 1) : [cur])]
+  }, []) : arr;
+}
+flat(arr)
+console.log("🚀 ~ flat(arr):", flat(arr))
+// [1, 2, 3, [4]]
+
+flat(arr, 1)
+console.log("🚀 ~ flat(arr, 1):", flat(arr, 1))
+// [1, 2, 3, [4]]
+
+flat(arr, 2)
+console.log("🚀 ~ flat(arr, 2):", flat(arr, 2))
+// [1, 2, 3, 4]
