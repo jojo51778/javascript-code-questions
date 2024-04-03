882c0594523d4adaa95120dcdd8e4c6d73c6b194
zhangwen.chen <623025156@qq.com>
2024-04-03 20:33:54 +0800

    feat: add debounce with leading and trailing

---
+++ js/medium/debouncewithtt.js
@@ -0,0 +1,30 @@
+
+/**
+ * @param {Function} func
+ * @param {number} wait
+ * @param {boolean} option.leading
+ * @param {boolean} option.trailing
+ */
+function debounce(func, wait, option = {leading: false, trailing: true}) {
+  let timer = null
+
+  return function(...args) {
+
+    let isInvoked = false
+    // if not cooling down and leading is true, invoke it right away
+    if (timer === null && option.leading) {
+      func.call(this, ...args)
+      isInvoked = true
+    }
+
+    // no matter what, timer needs to be reset
+    window.clearTimeout(timer)
+    timer = window.setTimeout(() => {
+      if (option.trailing && !isInvoked) {
+        func.call(this, ...args)
+      }
+
+      timer = null
+    }, wait)
+  }
+}
