efd66c61922d6c42ec7985c78cbdaa74c1a3e500
zhangwen.chen <623025156@qq.com>
2024-04-08 23:10:10 +0800

    feat: add findCorrespondingNode queue

--- js/simple/findCorrespondingNode.js
+++ js/simple/findCorrespondingNode.js
@@ -1,9 +1,25 @@
+// Recursive
 function findCorrespondingNode (rootA, rootB, target) {
-  if (rootA === target) {
-    return rootB
-  }
+  if (rootA === target) return rootB;
+
   for (let i = 0; i < rootA.children.length; i++) {
     const res = findCorrespondingNode(rootA.children[i], rootB.children[i], target);
     if (res) return res;
   }
+  return null;
+}
+// Queue
+function findCorrespondingNode (rootA, rootB, target) {
+  if (rootA === target) return rootB;
+  let queueA = [rootA];
+  let queueB = [rootB];
+
+  while(queueA.length) {
+    let currentElementA = queueA.shift();
+    let currentElementB = queueB.shift();
+    if (currentElementA === target) return currentElementB;
+    queueA.push(...currentElementA.children);
+    queueB.push(...currentElementB.children);
+  }
+  return null;
 }
